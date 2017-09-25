c8e9cd1d078801f411e81214b2038e04fd5d21da
Strube <revmydev@gmail.com>
2017-09-24 23:04:36 -0700

    changed COLS to ALL_COLS set up member var for keeping track of colors for single and dual color spirals created changeColor method created skeleton for drawSquareSpiral

--- Painter.py
+++ Painter.py
@@ -4,7 +4,7 @@ from random import randint
 
 
 class Painter:
-    def __init__(self, w, h, top_left_x, top_left_y, bottom_right_x, bottom_right_y):
+    def __init__(self, w, h, top_left_x, top_left_y, bottom_right_x, bottom_right_y, distance):
         # colors, row 1
         self.BLACK = [764, 60]
         self.GREY = [788, 60]
@@ -31,11 +31,14 @@ class Painter:
         # generalize these since they can vary
 
         # list of all colors above
-        self.COLS = [
+        self.ALL_COLORS = [
             self.BLACK, self.GREY, self.BROWN, self.RED, self.ORANGE, self.YELLOW, self.GREEN, self.LIGHT_BLUE,
             self.NAVY, self.PURPLE, self.WHITE, self.LIGHT_GREY, self.LIGHT_BROWN, self.PINK, self.LIGHT_ORANGE,
             self.LIGHT_YELLOW, self.LIGHT_GREEN, self.LIGHT_BLUE, self.PASTEL_BLUE, self.BLUE_GREY, self.LIGHT_PURPLE
                     ]
+
+        # color coordinates for single and dual color spirals
+        self.cols = []
 
         # width, height of canvas
         self.WIDTH = w
@@ -48,3 +51,20 @@ class Painter:
         # bottom right corner of canvas
         self.BR_X = bottom_right_x
         self.BR_Y = bottom_right_y
+
+        # initial spiral distance
+        self.distance = distance
+
+    # click a different color from the list of colors
+    def changeColor(self):
+        idx = randint(0, len(self.ALL_COLORS)-1)
+        pyautogui.click(self.ALL_COLORS[idx][0], self.ALL_COLORS[idx][1])
+
+    # draws a square spiral. numSpirals dictates the number of spirals to draw. if numSpirals is 0, it will draw spirals
+    # infinitely until interrupted. otherwise it will draw the number of spirals passed to drawMode. if numSpirals is
+    # not numeric, it will draw one spiral and stop.
+    # drawType will dictate if the spiral wil be one color, two colors, or potentially all the colors in the COLS list
+    # for drawType, 1 = 1 color, 2 = 2 colors, everything else will be full color range
+    def drawSquareSpiral(self, numSpirals, drawType):
+
+       
